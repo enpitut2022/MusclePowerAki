d31ebf95643fc21dd893a70d730bca79940cae4a
ProfessorFjord4 <professorfjord4@gmail.com>
2022-10-19 13:35:34 +0900

    index.htmlで表示できるようにし、データベースの設定を一部行った

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    name = "Hello World"
-    return name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///muscle.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
+db = SQLAlchemy(app)
+
+# データベースのmemberテーブルの定義
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 ## おまじない
 if __name__ == "__main__":
