24d594d3d395b103b3fb3b034b97e221ade3c7b8
jummmmma <jets0225jun@gmai.com>
2022-11-16 13:54:52 +0900

    複数チームの選択について

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 
@@ -18,6 +18,7 @@ class Member(db.Model):
     status = db.Column(db.String(10), default="start")
     days = db.Column(db.Integer, default=1)
     date = db.Column(db.DateTime)
+    teamid = db.Column(db.Integer)
 
 # データベースのcommentテーブルの定義
 class Comment(db.Model):
@@ -25,22 +26,37 @@ class Comment(db.Model):
     name = db.Column(db.String(64))
     date = db.Column(db.DateTime)
     comment = db.Column(db.String(256))
+    teamid = db.Column(db.Integer)
+
+# データベースのteamテーブルの定義
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    date = db.Column(db.DateTime)
+    description = db.Column(db.String(128))
 
 @app.route('/')
 def index():
+    team = Team.query.all()
+    return render_template('index.html', tm =team)
+
+@app.route('/detail/<int:id>')
+def detail(id):
+    teamdata = Team.query.get(id)
     # start状態のメンバー全員を取得
-    members = Member.query.filter_by(status = "start").order_by(Member.date).all()
+    members = Member.query.filter_by(teamid = id).filter_by(status = "start").order_by(Member.date).all()
     # start状態のメンバー数をカウント
     membersNumber = len(members)
     # 状態関係なくメンバー全員を取得
-    allmember = Member.query.all()
+    allmember = Member.query.filter_by(teamid = id).all()
     # コメントすべてを取得
-    comments = Comment.query.all()
+    comments = Comment.query.filter_by(teamid = id).all()
     # index.htmlに値を渡し、表示
-    return render_template('index.html', ms = members, mn = membersNumber, am = allmember, cm = comments)
+    return render_template('detail.html',td = teamdata, ms = members, mn = membersNumber, am = allmember, cm = comments)
 
 @app.route('/workstart', methods=["post"])
 def workstart():
+    teamid = int(request.form["teamid"])
     # 現在時刻の取得
     nowdate = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
     ## デバッグ用(1日経過させる)
@@ -52,7 +68,8 @@ def workstart():
     # DBに名前がなかった場合
     if memberSearch == None:
         # 基準となる時刻
-        baseDate = datetime.datetime(2022, 10, 28, 22, 0, 0)
+        teamsearch = Team.query.filter_by(id = teamid).first()
+        baseDate = teamsearch.date
         ## ↓デバッグ用(デモの日時)
         ## baseDate = datetime.datetime(2022, 10, 28, 12, 30, 0)
         # 基準となる時刻と現在時刻の差を取る(絶対値)
@@ -62,34 +79,36 @@ def workstart():
         # 秒数が1時間未満または23時間以上なら設定された日時の前後1時間以内とみなす
         if diff_a_sec < 60 * 60 or diff_a_sec >= 23 * 60 * 60:
             # 新しくメンバーを追加
-            newMember = Member(name=name, date=nowdate)
+            newMember = Member(name=name, date=nowdate, teamid=teamid)
             db.session.add(newMember)
     # DBに名前があった場合
     else:
+        if memberSearch.teamid == teamid:
         # その名前のメンバーのステータスがfinish(startでない)とき
-        if memberSearch.status == "finish":
-            # その名前のメンバーの最終運動日時を取得
-            lastdate = memberSearch.date
-            # 現在時刻と最終運動日時を比較
-            diff = nowdate - lastdate
-            # 1日と1時間(25時間以内)もしくは0日と23時間の範囲にdiffが収まったとき
-            # すなわち、最後に運動した日の翌日に運動するとき
-            if (diff.seconds <= 60 * 60 and diff.days == 1) or (diff.seconds >= 23 * 60 * 60 and diff.days == 0):
-                # そのMemberをstart状態にして連続日数を1追加し、最終運動日時を更新する
-                memberSearch.status = "start"
-                memberSearch.days += 1
-                memberSearch.date = nowdate
-            # diffの範囲が設定された時間の前後1時間に収まり、かつ最後に運動した日から2日以上経過しているとき
-            elif (diff.seconds <= 60 * 60 and diff.days >= 2) or (diff.seconds >= 23 * 60 * 60 and diff.days >= 1):
-                # そのMemberをstart状態にして連続日数を初期化し、最終運動日時を更新する
-                memberSearch.status = "start"
-                memberSearch.days = 1
-                memberSearch.date = nowdate
+            if memberSearch.status == "finish":
+                # その名前のメンバーの最終運動日時を取得
+                lastdate = memberSearch.date
+                # 現在時刻と最終運動日時を比較
+                diff = nowdate - lastdate
+                # 1日と1時間(25時間以内)もしくは0日と23時間の範囲にdiffが収まったとき
+                # すなわち、最後に運動した日の翌日に運動するとき
+                if (diff.seconds <= 60 * 60 and diff.days == 1) or (diff.seconds >= 23 * 60 * 60 and diff.days == 0):
+                    # そのMemberをstart状態にして連続日数を1追加し、最終運動日時を更新する
+                    memberSearch.status = "start"
+                    memberSearch.days += 1
+                    memberSearch.date = nowdate
+                # diffの範囲が設定された時間の前後1時間に収まり、かつ最後に運動した日から2日以上経過しているとき
+                elif (diff.seconds <= 60 * 60 and diff.days >= 2) or (diff.seconds >= 23 * 60 * 60 and diff.days >= 1):
+                    # そのMemberをstart状態にして連続日数を初期化し、最終運動日時を更新する
+                    memberSearch.status = "start"
+                    memberSearch.days = 1
+                    memberSearch.date = nowdate
     db.session.commit()
-    return redirect("/")
+    return redirect(url_for("detail",id =teamid))
 
 @app.route('/comment', methods=["post"])
 def addcomment():
+    teamid = int(request.form["teamid"])
     # 現在時刻の取得
     nowdate = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
     # 秒の小数点以下切り捨て
@@ -99,10 +118,12 @@ def addcomment():
     # formで入力されたコメント(comment)を受け取る
     comment = request.form["comment"]
     # 新しくコメントを追加
-    newComment = Comment(name=name, date=dt, comment=comment)
+    newComment = Comment(name=name, date=dt, comment=comment, teamid=teamid)
     db.session.add(newComment)
     db.session.commit()
-    return redirect("/")
+    return redirect(url_for("detail", id =teamid))
+
+
 
 ## おまじない
 if __name__ == "__main__":
