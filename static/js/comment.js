067855b14cb499d0f4610895c140dca68515113e
yananananarn <nagisa87@outlook.jp>
2022-12-16 16:53:03 +0900

    使用していない関数を消した

--- static/js/comment.js
+++ static/js/comment.js
@@ -1,17 +1,6 @@
-function display() {
-    const username = window.localStorage.getItem('username');
-    const chatname = document.getElementById("chat_username");
-
-    chatname.value = username;
-}
-
 function nameinput() {
     const username = window.localStorage.getItem('username');
     const commentname = document.getElementById("comment_username");
 
     commentname.value = username;
-}
-
-window.onload = function() {
-    display();
 }
